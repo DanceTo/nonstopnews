774d20ec11917840a7f00134f58e87c2a0324040
Mateo <dennisoon87@gmail.com>
2025-04-12 10:15:41 +0300

    wrk with newsCat page

---
+++ app/[lang]/[news]/page.tsx
@@ -0,0 +1,17 @@
+import type { Locale } from '@/i18n.config';
+
+export type NewsPageProps = {
+  params: Promise<{
+    lang: Locale; // 'uk' | 'ru'
+  }>;
+};
+
+export default async function NewsPage({ params }: NewsPageProps) {
+  const { lang } = await params;
+
+  return (
+    <section className={`section pb-[48px] pt-0 xl:pb-[100px] `}>
+      <div>Сторінка однієї новини!!!</div>
+    </section>
+  );
+}
