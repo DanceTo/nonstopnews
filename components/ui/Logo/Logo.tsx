a00e6da6e19d43ed74babbaff7a892800f9646e4
Mateo <dennisoon87@gmail.com>
2025-11-12 14:21:01 +0200

    upd1

--- components/ui/Logo/Logo.tsx
+++ components/ui/Logo/Logo.tsx
@@ -15,9 +15,9 @@ export const Logo: React.FC<LogoProps> = ({
   logoLeftText = '',
 }) => {
   const lang = useParams().lang as Locale;
-  const logoSvgStyles = classNames('h-[51px] w-[87px]', {
-    'xl:w-[300px] xl:h-[64px]': position === 'header',
-    'md:w-[300px] md:h-[64px]': position === 'footer' || 'contacts',
+  const logoSvgStyles = classNames('h-[48px] w-[48px] mb-0', {
+    'xl:w-[80px] xl:h-[80px] mb-[-20px]': position === 'header',
+    'md:w-[80px] md:h-[80px]': position === 'footer' || 'contacts',
   });
   const logoTextStyles = classNames({
     'container mb-4 flex max-w-fit items-center justify-center font-bold text-red2 xl:text-5xl':
@@ -29,7 +29,7 @@ export const Logo: React.FC<LogoProps> = ({
       href={`/${lang}`}
       className={`common-transition z-2 relative block max-w-max text-black-light hover:text-red focus:text-red ${className} ${logoTextStyles}`}
     >
-      <LogoSvg width={116} height={64} className={logoSvgStyles} />
+      <LogoSvg width={80} height={80} className={logoSvgStyles} />
     </Link>
   );
 };
